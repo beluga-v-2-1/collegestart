adb49620625584e6f053c8932a85d633c47fe079
Richard MR <109469713+beluga-v-2-1@users.noreply.github.com>
2023-10-29 13:44:19 +0530

    Update lab5.c

--- computer cia 3/lab5.c
+++ computer cia 3/lab5.c
@@ -1,84 +1,48 @@
 #include <stdio.h>
-#include <string.h>
 
-// Define the structure to represent an Employee
 struct Employee {
-    int employeeID;
+    int employee_id;
     char name[50];
-    float salary;
+    int age;
+    float basic_salary;
 };
 
-// Function to perform binary search on an array of employees by employee ID
-int binarySearch(struct Employee arr[], int left, int right, int targetID) {
+int binarySearch(struct Employee employees[], int n, int target_id) {
+    printf("***********************\n");
+    printf("Reg.No: 2362640\n");
+    printf("NAME: Tharun Adhithya.A\n");
+    printf("***********************\n");
+    int left = 0, right = n - 1;
     while (left <= right) {
-        int mid = left + (right - left) / 2;
-
-        // If the target ID is found, return the index
-        if (arr[mid].employeeID == targetID) {
-            return mid;
-        }
-
-        // If the target ID is less, search the left half
-        if (arr[mid].employeeID > targetID) {
-            right = mid - 1;
-        }
-
-        // If the target ID is greater, search the right half
-        else {
-            left = mid + 1;
-        }
+        int mid = (left + right) / 2;
+        if (employees[mid].employee_id == target_id)
+            return mid; // Employee found
+        (employees[mid].employee_id < target_id) ? (left = mid + 1) : (right = mid - 1);
     }
-
-    // If the target ID is not found, return -1
-    return -1;
+    return -1; // Employee not found
 }
 
 int main() {
-    printf("NAME :Tharun Adhithya.A\n")
-    printf("REGNO :- 2362640\n")
-    int n, targetID;
-    
-    // Input the number of employees
+    int n;
     printf("Enter the number of employees: ");
     scanf("%d", &n);
 
-    // Create an array of Employee structures
     struct Employee employees[n];
 
-    // Input employee details
     for (int i = 0; i < n; i++) {
-        printf("Enter details for Employee %d:\n", i + 1);
-        employees[i].employeeID = i + 1;
-        printf("Name: ");
-        scanf("%s", employees[i].name); // Assuming no spaces in the name
-        printf("Salary: ");
-        scanf("%f", &employees[i].salary);
+        printf("ID, Name, Age, Salary for Employee %d: ", i + 1);
+        scanf("%d %s %d %f", &employees[i].employee_id, employees[i].name, &employees[i].age, &employees[i].basic_salary);
     }
 
-    // Sort the array by employee ID (binary search requires a sorted array)
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (employees[i].employeeID > employees[j].employeeID) {
-                struct Employee temp = employees[i];
-                employees[i] = employees[j];
-                employees[j] = temp;
-            }
-        }
-    }
+    int target_id;
+    printf("Enter Employee ID to search: ");
+    scanf("%d", &target_id);
 
-    // Input the target employee ID for search
-    printf("Enter the Employee ID to search: ");
-    scanf("%d", &targetID);
+    int result = binarySearch(employees, n, target_id);
 
-    // Perform binary search
-    int result = binarySearch(employees, 0, n - 1, targetID);
-
-    // Display the search result
     if (result != -1) {
-        printf("Employee Found:\n");
-        printf("Employee ID: %d\n", employees[result].employeeID);
-        printf("Name: %s\n", employees[result].name);
-        printf("Salary: %.2f\n", employees[result].salary);
+        printf("Employee Found!\n");
+        printf("Employee ID: %d\nName: %s\nAge: %d\nBasic Salary: %.2f\n", employees[result].employee_id, employees[result].name, employees[result].age, employees[result].basic_salary);
     } else {
         printf("Employee not found.\n");
     }
