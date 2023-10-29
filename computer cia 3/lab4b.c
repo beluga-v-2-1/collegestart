8828726df228d79f9025a31eec1693081bebb33e
Richard MR <109469713+beluga-v-2-1@users.noreply.github.com>
2023-10-29 13:41:26 +0530

    Update lab4b.c

--- computer cia 3/lab4b.c
+++ computer cia 3/lab4b.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    printf("NAME :Tharun Adhithya.A\n")
-    printf("REGNO :- 2362640\n")
+    printf("***********************\n");
+    printf("Reg.No: 2362640\n");
+    printf("NAME: Tharun Adhithya.A\n");
+    printf("***********************\n");
     int rows = 3;
     int currentNumber = 5;
-    
     for (int i = 1; i <= rows; i++) {
         for (int j = 1; j <= i; j++) {
             printf("%d ", currentNumber);
