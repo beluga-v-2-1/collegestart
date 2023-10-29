03aea0151e6b514cf0bad3987cfa79aedf1cfa8c
Richard MR <109469713+beluga-v-2-1@users.noreply.github.com>
2023-10-29 13:34:41 +0530

    Update lab2.c

--- computer cia 3/lab2.c
+++ computer cia 3/lab2.c
@@ -1,56 +1,52 @@
 #include <stdio.h>
 
-// Function to calculate salary based on employee type, daily rate, and experience
-float calculateSalary(char employeeType, int experience) {
-    float dailyRate, increment;
-    printf("NAME :Richard MR\n")
-    printf("REGNO :- 2362528\n")
-    
-    if (employeeType == 'P') {
-        dailyRate = 1000.0;  // Daily rate for Permanent employees
-    } else if (employeeType == 'T') {
-        dailyRate = 400.0;   // Daily rate for Temporary employees
-    } else {
-        printf("Invalid Employee Type. Please enter 'P' for Permanent or 'T' for Temporary.\n");
-        return -1;  // Indicate an error
-    }
-
-    increment = 1000.0 * experience;
-    
-    return dailyRate * 365 + increment;
-}
-
 int main() {
-    int employeeID, age, experience;
-    char employeeType;
-    char department[50];
+    // Declare variables to store employee details
+    int employee_id, age, experience;
+    float basic_salary, da, tax, final_salary;
+    char employee_type, department[50];
 
+    // Input employee details
+    
     printf("Enter Employee ID: ");
-    scanf("%d", &employeeID);
-
+    scanf("%d", &employee_id);
     printf("Enter Age: ");
     scanf("%d", &age);
-
     printf("Enter Employee Type (P for Permanent, T for Temporary): ");
-    scanf(" %c", &employeeType);
-
+    scanf(" %c", &employee_type);
     printf("Enter Department: ");
     scanf("%s", department);
-
     printf("Enter Years of Experience: ");
     scanf("%d", &experience);
 
-    float salary = calculateSalary(employeeType, experience);
-
-    if (salary >= 0) {
-        printf("\nEmployee Salary Slip\n");
-        printf("Employee ID: %d\n", employeeID);
-        printf("Age: %d\n", age);
-        printf("Employee Type: %c\n", employeeType);
-        printf("Department: %s\n", department);
-        printf("Years of Experience: %d\n", experience);
-        printf("Salary: %.2f\n", salary);
+    // Calculate basic salary based on employee type
+    if (employee_type == 'P') {
+        basic_salary = 1000.0 + (1000.0 * experience);
+    } else if (employee_type == 'T') {
+        basic_salary = 400.0 + (1000.0 * experience);
+    } else {
+        printf("Invalid employee type.\n");
+        return 1; // Exit with an error code
     }
+
+    // Calculate DA and Tax
+    da = 0.1 * basic_salary;
+    tax = 0.2 * basic_salary + da;
+
+    // Calculate Final Salary
+    final_salary = basic_salary + da - tax;
+
+    // Display Employee Salary Slip
+    printf("\nEmployee Salary Slip\n");
+    printf("Employee ID: %d\n", employee_id);
+    printf("Age: %d\n", age);
+    printf("Basic Salary: %.2f\n", basic_salary);
+    printf("Employee Type: %s\n", (employee_type == 'P') ? "Permanent" : "Temporary");
+    printf("Department: %s\n", department);
+    printf("Years of Experience: %d\n", experience);
+    printf("DA: %.2f\n", da);
+    printf("Tax: %.2f\n", tax);
+    printf("Final Salary: %.2f\n", final_salary);
 
     return 0;
 }
